fc14c588e86f9f4b4c5fe63283304a900e3bc6d7
Jared Engler <jared.lite@gmail.com>
2020-12-11 19:36:37 -0600

    use static import
    src/App.js
      - use static import on React.Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import './App.css'
 import TodoItem from './TodoItem'
 import todoData from './todoData'
 
-class App extends React.Component {
+class App extends Component {
 
     todoItems() {
         return todoData.map(t => <TodoItem key={t.key} data={t} />)
